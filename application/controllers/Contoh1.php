843d68d2899ceab4939eb2a9cb3f97b2728c7745
bagastriwibowo12345 <Bagastriwibowo999@gmail.com>
2023-03-26 13:43:11 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php 
+class contoh1 extends CI_Controller
+{ 
+   public function index()
+   {
+   	  echo "<h1>Perkenalkan</h1>";
+   	  echo"Nama saya Bagas Tri Wibowo
+   	  	   Saya tinggal di daerah bekasi olahraga yang saya sukai adalah bermain bola";
+
+   }
+}
